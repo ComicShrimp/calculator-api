a43cc443a0aeeea0b2c4765b96d551aabbe2f6d6
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-04-19 21:49:42 -0300

    feat: adiciona operação de soma

--- calculator-api/main.py
+++ calculator-api/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Query
+
+from .models import Resultado
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root() -> dict:
+    return {"versao": "0.1.0"}
+
+
+@app.get("/soma", response_model=Resultado)
+async def soma(number: List[float] = Query(None)) -> Resultado:
+    return Resultado(resultado=sum(number))
---
+++ calculator-api/models.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+
+class Resultado(BaseModel):
+    resultado: float
