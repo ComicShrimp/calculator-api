ee83fc9f00e8eae04d51a35736867c4618f86b79
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-04-19 23:44:08 -0300

    feat: adiciona operacao de subtração

--- calculator_api/functions.py
+++ calculator_api/functions.py
@@ -3,3 +3,7 @@ from typing import List
 
 def operacao_soma(numeros: List[float]) -> float:
     return sum(numeros)
+
+
+def operacao_subtracao(numeros: List[float]) -> float:
+    return sum(numeros) * -1
--- calculator_api/main.py
+++ calculator_api/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import FastAPI, HTTPException, Query
 
-from calculator_api.functions import operacao_soma
+from calculator_api.functions import operacao_soma, operacao_subtracao
 from calculator_api.models import Resultado, Versao
 
 app = FastAPI(
@@ -21,5 +21,13 @@ async def versao() -> Versao:
 async def soma(numeros: List[float] = Query(None)) -> Resultado:
     try:
         return Resultado(resultado=operacao_soma(numeros=numeros))
+    except TypeError:
+        raise HTTPException(status_code=422, detail="Nenhum numero informado!")
+
+
+@app.get("/subtracao", response_model=Resultado)
+async def subtracao(numeros: List[float] = Query(None)) -> Resultado:
+    try:
+        return Resultado(resultado=operacao_subtracao(numeros=numeros))
     except TypeError:
         raise HTTPException(status_code=422, detail="Nenhum numero informado!")
