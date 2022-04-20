f20cdf3876f9375b9c49d6731bb351d0551a157a
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-04-19 23:12:02 -0300

    feat: adiciona pipeline

---
+++ calculator-api/functions.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def operacao_soma(numeros: List[float]) -> float:
+    return sum(numeros)
--- calculator-api/main.py
+++ calculator-api/main.py
@@ -1,17 +1,25 @@
 from typing import List
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, HTTPException, Query
 
+from .functions import operacao_soma
 from .models import Resultado
 
-app = FastAPI()
+app = FastAPI(
+    title="Calculator API",
+    description="Projeto destinado a ser utilizado no TCC de Eng. Computação",
+    version="0.1.0",
+)
 
 
 @app.get("/")
 async def root() -> dict:
-    return {"versao": "0.1.0"}
+    return {"versao": app.version}
 
 
 @app.get("/soma", response_model=Resultado)
-async def soma(number: List[float] = Query(None)) -> Resultado:
-    return Resultado(resultado=sum(number))
+async def soma(numeros: List[float] = Query(None)) -> Resultado:
+    try:
+        return Resultado(resultado=operacao_soma(numeros=numeros))
+    except TypeError:
+        raise HTTPException(status_code=422, detail="Nenhum numero informado!")
