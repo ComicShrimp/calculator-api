6406c5052924d13ba82f2e4dac13f8a66dc9b037
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2022-05-16 23:27:42 -0300

    test: adiciona testes

--- calculator_api/tests/unidade/test_soma_unidade.py
+++ calculator_api/tests/unidade/test_soma_unidade.py
@@ -3,22 +3,22 @@ import pytest
 from calculator_api.functions import operacao_soma
 
 
-def test_soma_numeros_positivos() -> None:
+def test_numeros_positivos() -> None:
     numeros = [2.0, 2.0]
     assert operacao_soma(numeros=numeros) == 4
 
 
-def test_soma_numeros_negativos() -> None:
+def test_numeros_negativos() -> None:
     numeros = [-2.0, -2.0]
     assert operacao_soma(numeros=numeros) == -4
 
 
-def test_soma_numeros_negativos_e_positivos() -> None:
+def test_numeros_negativos_e_positivos() -> None:
     numeros = [-2.0, 2.0]
     assert operacao_soma(numeros=numeros) == 0
 
 
-def test_erro_soma_string() -> None:
+def test_erro_string() -> None:
     numeros = ["texto", -2.0]
     with pytest.raises(TypeError):
         operacao_soma(numeros=numeros)  # type: ignore
